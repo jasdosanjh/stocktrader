d05dde8e391ce508b556d53ee9a1c1b3910a85e6
jazdosanjh <jas@dosanjh.co.uk>
2019-01-17 17:54:51 +0000

    Created navbar/header for the project to work with. I'll amend the styles later.#

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <app-header></app-header>
+    <div class="row">
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-
+  components: {
+    appHeader: Header
+  }
 }
 </script>
 
 <style lang="scss">
+body {
+  background: #EEE;
+}
 
+h1 {
+  padding-top: 30px;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>The Header Component</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand" href="#">Stock Trader</router-link>
+    
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+          <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+      </ul>
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a class="nav-link" href="#">End Day</a></li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Save Data</a>
+          <a class="dropdown-item" href="#">Load Data</a>
+        </div>
+      </li>
+      </ul>
+      </form>
+    </div>
+  </nav>
 </template>
+
+<style>
+    li {
+        padding: 0 10px;
+    }
+    a {
+        color: black;
+    }
+
+    a:hover {
+        color: black;
+        text-decoration: none;
+    }
+</style>
