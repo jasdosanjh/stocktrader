ecbe5a6bdd999529ebeab8319c1ea3a4c32071dd
jazdosanjh <jas@dosanjh.co.uk>
2019-01-17 15:27:17 +0000

    Setup project routes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="container">
-    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+
 }
 </script>
 
