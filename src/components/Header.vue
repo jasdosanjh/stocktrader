be6f2220457cafb37a4c7890749ad4e6f1d74c0f
jazdosanjh <jas@dosanjh.co.uk>
2019-01-17 19:47:38 +0000

    Changed to Bootstrap 3, and also amended the header, and created the stocks component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,32 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand" href="#">Stock Trader</router-link>
-    
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav mr-auto">
-          <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
-          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
-      </ul>
-      <ul class="navbar-nav navbar-right">
-        <li class="nav-item"><a class="nav-link" href="#">End Day</a></li>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Save Data</a>
-          <a class="dropdown-item" href="#">Load Data</a>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" @click="endDay">End Day</a></li>
+                    <li class="dropdown">
+                        <a
+                                href="#"
+                                class="dropdown-toggle"
+                                data-toggle="dropdown"
+                                role="button"
+                                aria-haspopup="true"
+                                aria-expanded="false">Save & Load <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Save Data</a></li>
+                            <li><a href="#">Load Data</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </li>
-      </ul>
-      </form>
-    </div>
-  </nav>
+    </nav>
 </template>
-
-<style>
-    li {
-        padding: 0 10px;
-    }
-    a {
-        color: black;
-    }
-
-    a:hover {
-        color: black;
-        text-decoration: none;
-    }
-</style>
