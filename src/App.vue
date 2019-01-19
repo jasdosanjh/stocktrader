c9c9ac2303bac54d31c53daa186c6a3281c78dca
jazdosanjh <jas@dosanjh.co.uk>
2019-01-19 19:36:11 +0000

    Animated route transitions with CSS

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div class="container">
     <app-header></app-header>
     <div class="row">
-        <div>
-            <router-view></router-view>
+        <div class="col-xs-12">
+            <transition name="slide" mode="out-in">
+              <router-view></router-view>
+            </transition>
         </div>
     </div>
   </div>
@@ -23,6 +25,36 @@ export default {
 
 <style lang="scss">
 body {
-  background: #EEE;
+  padding: 30px;
+}
+
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
