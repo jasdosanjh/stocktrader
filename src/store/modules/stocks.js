f7e4504ffc4508869f134358a2ccd35c05bd1641
jazdosanjh <jas@dosanjh.co.uk>
2019-01-19 19:28:06 +0000

    Created the end day button, and logic to randomize the stocks

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -7,6 +7,11 @@ const state = {
 const mutations = {
     'SET_STOCKS' (state, stocks) {
         state.stocks = stocks;
+    },
+    'RND_STOCKS' (state) {
+        state.stocks.forEach(stock => {
+            stock.price = Math.round(stock.price * (1 + Math.random() - 0.5));
+        });
     }
 };
 
